7ef902563d53c3e027dc05bd91cfeac2716a7be6
Viktor Blyakherov <xxxxxvic@yandex.ru>
2022-07-18 21:44:17 +0300

    3. Интерфейс Input [#181092 #322178] rev2

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -3,16 +3,18 @@ package ru.job4j.poly;
 public class Bus implements Transport {
     @Override
     public void drive() {
-
+        System.out.println("I'm drive");
     }
 
     @Override
     public void passengers(int countPassengers) {
-
+        System.out.println("Driving with " + countPassengers + " passengers");
     }
 
     @Override
     public float fill(int volumeToFill) {
-        return 0;
+        float fuelPrice = 52.40f;
+        System.out.println("I'm filling");
+        return volumeToFill * fuelPrice;
     }
 }
