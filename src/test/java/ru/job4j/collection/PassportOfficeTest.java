3b71fc33d0eac815d164b9bc5d80412c8ebaa565
Viktor Blyakherov <xxxxxvic@yandex.ru>
2022-07-21 20:59:47 +0300

    1. Паспорт и жители [#212656]

---
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class PassportOfficeTest {
+
+    @Test
+    public void add() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+    @Test
+    public void whenAddDuplicate() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        Citizen citizen1 = new Citizen("2f44a", "Ivan Ivanov");
+        assertFalse(office.add(citizen1));
+    }
+
+}
