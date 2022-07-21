b7d992857019d268266a3c95be3dd6cf6b4e42fc
Viktor Blyakherov <xxxxxvic@yandex.ru>
2022-07-21 20:19:36 +0300

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("111@222.com", "John John");
+        map.put("333@444.com", "Joe Joe");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
