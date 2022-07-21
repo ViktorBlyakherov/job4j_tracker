7b1047ec4dba2de2c1c7d38250988183c8cb3630
Viktor Blyakherov <xxxxxvic@yandex.ru>
2022-07-21 21:44:16 +0300

    3. Банковские переводы [#10038]

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,53 @@
+package ru.job4j.bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private final Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        users.putIfAbsent(user, new ArrayList<>());
+    }
+
+    public void addAccount(String passport, Account account) {
+        users.get(findByPassport(passport)).add(account);
+    }
+
+    public User findByPassport(String passport) {
+        for (User user : users.keySet()) {
+            if (passport.equals(user.getPassport())) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        List<Account> accounts = users.get(findByPassport(passport));
+        if (accounts == null) {
+            return null;
+        }
+        for (Account account : accounts) {
+            if (requisite.equals(account.getRequisite())) {
+                return account;
+            }
+        }
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+
+        Account accountSrc = findByRequisite(srcPassport, srcRequisite);
+        Account accountDest = findByRequisite(destPassport, destRequisite);
+        if (accountSrc == null || accountDest == null || accountSrc.getBalance() < amount) {
+            return false;
+        }
+        accountSrc.setBalance(accountSrc.getBalance() - amount);
+        accountDest.setBalance(accountDest.getBalance() + amount);
+        return true;
+    }
+}
