b8e53f3f88211490e3be96ea2c6ff075ac4f1379
Viktor Blyakherov <xxxxxvic@yandex.ru>
2022-07-22 13:51:29 +0300

    1. Телефонный справочник на базе ArrayList [#41598 #324520] rev2

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -26,6 +26,6 @@ public class PhoneDictionaryTest {
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
         ArrayList<Person> persons = phones.find("Kolya");
-        assertThat(persons.isEmpty(), is(true));
+        assertTrue(persons.isEmpty());
     }
 }
