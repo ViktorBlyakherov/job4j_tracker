86ddb31f54975fcfc6093347b9e5ab200af2dd09
Viktor Blyakherov <xxxxxvic@yandex.ru>
2022-07-28 20:31:24 +0300

    2. Отсортировать департаменты [#34352 #326016] rev2

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -7,12 +7,7 @@ public class DepDescComp implements Comparator<String> {
     public int compare(String o1, String o2) {
         String[] array1 = o1.split("/");
         String[] array2 = o2.split("/");
-        int rsl;
-        if (array1[0].equals(array2[0])) {
-            rsl = o1.substring(array1[0].length()).compareTo(o2.substring(array2[0].length()));
-        } else {
-            rsl = array2[0].compareTo(array1[0]);
-        }
-        return rsl;
+        int rsl = array2[0].compareTo(array1[0]);
+        return rsl == 0 ? o1.compareTo(o2) : rsl;
     }
 }
--- src/main/java/ru/job4j/collection/Departments.java
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -9,11 +9,7 @@ public class Departments {
         for (String value : deps) {
             String start = "";
             for (String el : value.split("/")) {
-                if (start.equals("")) {
-                    start = start + el;
-                } else {
-                    start = start + "/" + el;
-                }
+                start += "".equals(start) ? el : "/" + el;
                 tmp.add(start);
             }
         }
